dc8a1395bf9b8b415cd643a8264c7919290f0d30
nkhoyt3-design <nkhoyt3@gmail.com>
2026-02-19 16:26:52 -0500

    Modify USER table definition
    Removed check constraint on Bdate in USER table.

--- CS4400 Phase II A Template.sql
+++ CS4400 Phase II A Template.sql
@@ -30,11 +30,11 @@ use media_streaming_service;
 data types, and check constraints) here.  You may sequence them in any order that 
 works for you (and runs successfully). */
 
-create table USER (AccountID int primary key, 
+create table USER (
+  AccountID int primary key, 
   name varchar(100) not null, 
   Bdate Date not null, 
-  email varchar(150) not null unique, 
-  check (Bdate > CURRENT_DATE));
+  email varchar(150) not null unique);
 
 
 
